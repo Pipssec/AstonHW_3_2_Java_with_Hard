dffa6f837645b4afd6606e7c92321a3d1368513d
Artsem Stsepanenka <asstep@mail.ru>
2023-03-26 20:11:08 +0300

    finished hard*

--- app/src/main/java/com/example/homework_3_2/MainActivity.java
+++ app/src/main/java/com/example/homework_3_2/MainActivity.java
@@ -2,18 +2,26 @@ package com.example.homework_3_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.net.Uri;
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
-import com.example.homework_3_2.databinding.ActivityMainBinding;
-import com.squareup.picasso.Picasso;
 
+import com.example.homework_3_2.databinding.ActivityMainBinding;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView imageLoadLib;
+    ProgressDialog mProgressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,26 +29,48 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         Button loadButton = binding.openWebsiteButton;
-        ImageView imageLoadLibrary = binding.imageViewPictureLibrary;
+        imageLoadLib = binding.imageViewPictureLibrary;
         EditText editTextUrl = binding.websiteEdittext;
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String url = editTextUrl.getText().toString();
-                Uri picture = Uri.parse(url);
-                Picasso.Builder builder = new Picasso.Builder(MainActivity.this);
-                builder.listener(new Picasso.Listener()
-                {
-                    @Override
-                    public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception)
-                    {
-                        exception.printStackTrace();
-                        Toast toast = Toast.makeText(MainActivity.this, "Image load failed", Toast.LENGTH_LONG);
-                        toast.show();
-                    }
-                });
-                builder.build().load(picture).into(imageLoadLibrary);
+                new DownloadImage().execute(editTextUrl.getText().toString());
             }
         });
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialog = new ProgressDialog(MainActivity.this);
+            mProgressDialog.setTitle("Download Image");
+            mProgressDialog.setMessage("Loading...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Bitmap doInBackground(String... URL) {
+            String imageURL = URL[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream input = new java.net.URL(imageURL).openStream();
+                bitmap = BitmapFactory.decodeStream(input);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            if (bitmap == null) {
+                Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_SHORT).show();
+            }
+            imageLoadLib.setImageBitmap(bitmap);
+            mProgressDialog.dismiss();
+        }
     }
 }
